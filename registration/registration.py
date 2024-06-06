f5dcc8a80346d1d8f28bd81ef259fe3cd888e540
Shubham Kumar <skumar@monarchtractor.com>
2024-06-06 18:45:11 +0530

    multi-view-icp refactoring

--- registration/registration.py
+++ registration/registration.py
@@ -16,7 +16,6 @@ else:
 # TO-DO: 
 # robust icp -> outlier elimination
 # downsample target pcl
-# try croppping
 # try arun's method
 # try manual initial alignment / global registration methods
 
@@ -25,17 +24,17 @@ def draw_registration_result(source, target, transformation):
 	source_temp = source.clone()
 	target_temp = target.clone()
 
-	source_temp.paint_uniform_color([0.0, 0.0, 0.0])  # Red color
+	source_temp.paint_uniform_color([1.0, 0.0, 0.0])  # Red color
 	source_temp.transform(transformation) 
 
-	# target_temp.paint_uniform_color([0.0, 1.0, 0.0])  # Green color
+	target_temp.paint_uniform_color([0.0, 1.0, 0.0])  # Green color
 
 	# source_b.paint_uniform_color([0.0, 0.0,1.0])  # Blue color
 	
 	# source_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=200, origin=[0, 0, 0])
 	# target_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=50, origin=[0, 0, 0])
 
-	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=20, origin=[0, 0, 0])
+	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=2, origin=[0, 0, 0])	
 	# frame2 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=20, origin=[60, 0, 0])
 	
 	# tf1 = np.asarray([[1.0, 0.0 , 0,  0],
@@ -58,12 +57,13 @@ def draw_registration_result(source, target, transformation):
 		source_temp.to_legacy(),
 		#  source_b.to_legacy(),
 		target_temp.to_legacy(),
-		 frame1
+		#  frame1
 		#  frame2
 		],
 		zoom=0.4459,
 		front=[0.9288, -0.2951, -0.2242],
-		lookat=[1.6784, 2.0612, 1.4451],
+		# lookat=[1.6784, 2.0612, 1.4451],
+		lookat=[0, 0, 0],
 		up=[-0.3402, -0.9189, -0.1996])
 
 def prepare_pcd(pcd, min_bound, max_bound):
@@ -78,101 +78,39 @@ def prepare_pcd(pcd, min_bound, max_bound):
 	
 	return pcd_temp		
 
-
-if __name__ == "__main__":
-	
-	lidar_path = "../lidar2ply/lidar_0.ply"
-	svo_path = "../svo2ply/svo_0.ply"
-	
-	print(f"{o3d.__DEVICE_API__}")
-
-	# run_vanilla_icp(lidar_ply, svo_ply)
-	# print(f"lidar_pcl_span: {get_pcl_aabb(o3d.io.read_point_cloud(lidar_path))}")
-	# print(f"svo_pcl_span: {get_pcl_aabb(o3d.io.read_point_cloud(svo_path))}")	
-
-	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=2, origin=[0, 0, 0])	
-
+def run_multiview_icp(source, target):
 	s = time.time()
 
-	source = o3d.t.io.read_point_cloud(lidar_path)	
-	target = o3d.t.io.read_point_cloud(svo_path)	
-
-	source = prepare_pcd(source, [-5.0, -0.6, -50.0], [2.0 , 0.6, 10.0])
-	target = prepare_pcd(target, [-0.7, -10, -50.0], [0.8, 0.5, 2.0])
-
-	# source_num_points = np.asarray(source.to_legacy().points).shape[0]
-	# # target_num_points = np.asarray(target.to_legacy().points).shape[0]
-	# print(f"[BEFORE CROPPING] Number of points in source: {source_num_points}")
-	# # print(f"[BEFORE CROPPING] Number of points in target: {target_num_points}")
-		
-	# source_min_bound = [-5.0, -0.6, -50.0]
-	# source_max_bound = [2.0 , 0.6, 10.0]
-	# source_bbox = o3d.t.geometry.AxisAlignedBoundingBox(source_min_bound, source_max_bound)
-
-	# # target_min_bound = [-0.7, -10, -50.0]
-	# # target_max_bound = [0.8, 0.5, 2.0]
-	# # target_bbox = o3d.t.geometry.AxisAlignedBoundingBox(target_min_bound, target_max_bound)
-
-	# source = source.crop(source_bbox) 
-	# # target = target.crop(target_bbox) 
-	
-
-	# source_num_points = np.asarray(source.to_legacy().points).shape[0]
-	# # target_num_points = np.asarray(target.to_legacy().points).shape[0]
-	# print(f"[AFTER CROPPING] Number of points in source: {source_num_points}")
-	# # print(f"[AFTER CROPPING] Number of points in source: {target_num_points}")
-
-	o3d.visualization.draw_geometries([source.to_legacy(),target.to_legacy(), frame1])	
-	
-	
-	
-	exit(0)
-
-
-
-
+	print("[AFTER DOWNSAMPLING] Number of points in source: ", np.asarray(source.to_legacy().points).shape[0])
+	print("[AFTER DOWNSAMPLING] Number of points in target: ", np.asarray(target.to_legacy().points).shape[0])
 
 	source_cuda = source.cuda(0)
 	target_cuda = target.cuda(0)
-
 	
-
-
-	
-	
-	# Downsampling the svo pcl
-	voxel_size = 0.1  # Adjust the voxel size as needed
-	downsampled_target_cuda = target_cuda.voxel_down_sample(voxel_size)
-	o3d.visualization.draw_geometries([downsampled_target_cuda.to_legacy()])
-
-
-
-
-	exit(0)
 	target_cuda.estimate_normals(max_nn=30, radius=0.1)
-	downsampled_target_cuda.estimate_normals(max_nn=30, radius=0.1)
-
-	voxel_sizes = o3d.utility.DoubleVector([0.01, 0.0009, 0.00089])
+	
+	voxel_sizes = o3d.utility.DoubleVector([0.01, 0.001, 0.0001])
 
 	criteria_list = [
 		treg.ICPConvergenceCriteria(relative_fitness=0.001,
-									relative_rmse=0.0001,
-									max_iteration=100),
-		treg.ICPConvergenceCriteria(0.0001, 0.0001, 20),
+									relative_rmse=0.00001,
+									max_iteration=500),
+		treg.ICPConvergenceCriteria(0.0001, 0.0000001, 100),
 		treg.ICPConvergenceCriteria(0.001, 0.001, 5)
 	]
 
 	# `max_correspondence_distances` for Multi-Scale ICP (o3d.utility.DoubleVector):
-	max_correspondence_distances = o3d.utility.DoubleVector([100, 100, 100])
+	max_correspondence_distances = o3d.utility.DoubleVector([10, 0.01, 0.001])
 
 	init_source_to_target = o3d.core.Tensor.eye(4, o3d.core.Dtype.Float32)
 
-	estimation = treg.TransformationEstimationPointToPlane()
+	# estimation = treg.TransformationEstimationPointToPlane()
+	# estimation = treg.TransformationEstimationPointToPoint()
 
-	# mu, sigma = 0, 1.0
-	# estimation = treg.TransformationEstimationPointToPlane(
-    # treg.robust_kernel.RobustKernel(
-    #     treg.robust_kernel.RobustKernelMethod.CauchyLoss, sigma))
+	mu, sigma = 0, 0.1 # mean and standard deviation
+	estimation = treg.TransformationEstimationPointToPlane(
+    treg.robust_kernel.RobustKernel(
+        treg.robust_kernel.RobustKernelMethod.CauchyLoss, sigma))
 
 	# Save iteration wise `fitness`, `inlier_rmse`, etc. to analyse and tune result.
 	callback_after_iteration = lambda loss_log_map : print("Iteration Index: {}, Scale Index: {}, Scale Iteration Index: {}, Fitness: {}, Inlier RMSE: {},".format(
@@ -194,3 +132,26 @@ if __name__ == "__main__":
 	print("Inlier RMSE: ", registration_ms_icp.inlier_rmse)
 
 	draw_registration_result(source, target, registration_ms_icp.transformation) 	
+
+if __name__ == "__main__":
+	
+	lidar_path = "../lidar2ply/lidar_0.ply"
+	svo_path = "../svo2ply/svo_0.ply"
+	
+	print(f"{o3d.__DEVICE_API__}")
+
+	# run_vanilla_icp(lidar_ply, svo_ply)
+	# print(f"lidar_pcl_span: {get_pcl_aabb(o3d.io.read_point_cloud(lidar_path))}")
+	# print(f"svo_pcl_span: {get_pcl_aabb(o3d.io.read_point_cloud(svo_path))}")	
+
+	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=2, origin=[0, 0, 0])	
+
+	s = time.time()
+
+	source = o3d.t.io.read_point_cloud(lidar_path)	
+	target = o3d.t.io.read_point_cloud(svo_path)	
+
+	source = prepare_pcd(source, [-5.0, -0.6, -50.0], [2.0 , 0.6, 10.0])
+	target = prepare_pcd(target, [-0.7, -10, -50.0], [0.8, 0.5, 2.0])
+	
+	run_multiview_icp(source, target)	
