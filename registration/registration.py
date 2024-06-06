dbce7ed8fe5a982f494bf2668f2f59674d29f1e1
Shubham Kumar <skumar@monarchtractor.com>
2024-06-07 02:00:27 +0530

    saving

--- registration/registration.py
+++ registration/registration.py
@@ -6,7 +6,7 @@ import copy
 import time
 import math
 import fast_global_registration 
-
+import utils
 
 if o3d.__DEVICE_API__ == 'cuda':
 	import open3d.cuda.pybind.t.pipelines.registration as treg
@@ -14,10 +14,16 @@ else:
 	import open3d.cpu.pybind.t.pipelines.registration as treg
 
 # TO-DO: 
+# try plane fitting to tractor hood pcd
+# play with tractor hood pcd + one crop row on each side
 # robust icp -> outlier elimination
 # downsample target pcl
-# try arun's method
+# trying pre-processing for zed pointcloud
+# use pcd.segment_plane
+
 # try manual initial alignment / global registration methods
+
+
 
 def draw_registration_result(source, target, transformation):
 
@@ -34,7 +40,7 @@ def draw_registration_result(source, target, transformation):
 	# source_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=200, origin=[0, 0, 0])
 	# target_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=50, origin=[0, 0, 0])
 
-	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=2, origin=[0, 0, 0])	
+	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])	
 	# frame2 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=20, origin=[60, 0, 0])
 	
 	# tf1 = np.asarray([[1.0, 0.0 , 0,  0],
@@ -57,7 +63,7 @@ def draw_registration_result(source, target, transformation):
 		source_temp.to_legacy(),
 		#  source_b.to_legacy(),
 		target_temp.to_legacy(),
-		#  frame1
+		 frame1
 		#  frame2
 		],
 		zoom=0.4459,
@@ -89,18 +95,18 @@ def run_multiview_icp(source, target):
 	
 	target_cuda.estimate_normals(max_nn=30, radius=0.1)
 	
-	voxel_sizes = o3d.utility.DoubleVector([0.01, 0.001, 0.0001])
+	voxel_sizes = o3d.utility.DoubleVector([0.001, 0.0001, 0.00001])
 
 	criteria_list = [
 		treg.ICPConvergenceCriteria(relative_fitness=0.001,
-									relative_rmse=0.00001,
+									relative_rmse=0.001,
 									max_iteration=500),
-		treg.ICPConvergenceCriteria(0.0001, 0.0000001, 100),
-		treg.ICPConvergenceCriteria(0.001, 0.001, 5)
+		treg.ICPConvergenceCriteria(0.0001, 0.0001, 100),
+		treg.ICPConvergenceCriteria(0.001, 0.0001, 5)
 	]
 
 	# `max_correspondence_distances` for Multi-Scale ICP (o3d.utility.DoubleVector):
-	max_correspondence_distances = o3d.utility.DoubleVector([10, 0.01, 0.001])
+	max_correspondence_distances = o3d.utility.DoubleVector([100, 1, 0.1 ])
 
 	init_source_to_target = o3d.core.Tensor.eye(4, o3d.core.Dtype.Float32)
 
@@ -144,14 +150,27 @@ if __name__ == "__main__":
 	# print(f"lidar_pcl_span: {get_pcl_aabb(o3d.io.read_point_cloud(lidar_path))}")
 	# print(f"svo_pcl_span: {get_pcl_aabb(o3d.io.read_point_cloud(svo_path))}")	
 
-	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=2, origin=[0, 0, 0])	
+	frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])	
 
 	s = time.time()
 
 	source = o3d.t.io.read_point_cloud(lidar_path)	
 	target = o3d.t.io.read_point_cloud(svo_path)	
 
-	source = prepare_pcd(source, [-5.0, -0.6, -50.0], [2.0 , 0.6, 10.0])
-	target = prepare_pcd(target, [-0.7, -10, -50.0], [0.8, 0.5, 2.0])
+	# source = prepare_pcd(source, [-5.0, -0.3, -1.0], [2.0 , 0.325, -0.75])
+	# target = prepare_pcd(target, [-0.3, -10, -50.0], [0.4, 0.5, 2.0])
+	# target = prepare_pcd(target, [-0.3, 0.3, -50.0], [0.4, 0.4, 2.0])
 	
-	run_multiview_icp(source, target)	
+	
+	# target = target.voxel_down_sample(voxel_size=0.02)
+	# target.paint_uniform_color([0.0, 1.0, 0.0])	
+
+	
+	
+	o3d.visualization.draw_geometries([ target.to_legacy(), frame1])	
+	# o3d.visualization.draw_geometries([ source.to_legacy(), frame1])	
+	# o3d.visualization.draw_geometries([ source.to_legacy(), target.to_legacy(), frame1])	
+
+	# run_multiview_icp(source, target)
+
+	
--- registration/utils.py
+++ registration/utils.py
@@ -2,36 +2,97 @@
 
 import numpy as np
 import math
+import time
+import open3d as o3d
+
+if o3d.__DEVICE_API__ == 'cuda':
+	import open3d.cuda.pybind.t.pipelines.registration as treg
+else:
+	import open3d.cpu.pybind.t.pipelines.registration as treg
+
+
+def create_transformation_matrix(R, t):
+    """
+    Create a 4x4 transformation matrix from a 3x3 rotation matrix and 3x1 translation vector.
+    """
+    T = np.eye(4)  # Create a 4x4 identity matrix
+    T[:3, :3] = R  # Set the rotation matrix part
+    T[:3, 3] = t.squeeze()  # Set the translation vector part
+
+    return T
+
 
 def arun(A, B):
-    """Solve 3D registration using Arun's method: B = RA + t
-    """
-    N = A.shape[1]
+    """Solve 3D registration using Arun's method: B = RA + t"""
+    N = A.shape[1]  # Number of points
     assert B.shape[1] == N
 
-    # calculate centroids
-    A_centroid = np.reshape(1/N * (np.sum(A, axis=1)), (3,1))
-    B_centroid = np.reshape(1/N * (np.sum(B, axis=1)), (3,1))
+    # Calculate centroids
+    A_centroid = np.mean(A, axis=0).reshape(1, 3)
+    B_centroid = np.mean(B, axis=0).reshape(1, 3)
 
-    # calculate the vectors from centroids
+    # Calculate the vectors from centroids
     A_prime = A - A_centroid
     B_prime = B - B_centroid
 
-    # rotation estimation
+    # Rotation estimation
     H = np.zeros([3, 3])
     for i in range(N):
-        ai = A_prime[:, i]
-        bi = B_prime[:, i]
-        H = H + np.outer(ai, bi)
+        ai = A_prime[i, :]  # Extract the ith row (shape (3,))
+        bi = B_prime[i, :]  # Extract the ith row (shape (3,))
+        H += np.outer(ai, bi)
+
     U, S, V_transpose = np.linalg.svd(H)
     V = np.transpose(V_transpose)
     U_transpose = np.transpose(U)
     R = V @ np.diag([1, 1, np.linalg.det(V) * np.linalg.det(U_transpose)]) @ U_transpose
 
-    # translation estimation
-    t = B_centroid - R @ A_centroid
+    # Translation estimation
+    t = B_centroid.T - R @ A_centroid.T
 
     return R, t
+
+
+
+# def arun(A, B):
+# 	"""Solve 3D registration using Arun's method: B = RA + t
+# 	"""
+# 	print(f"Inside arun!")
+	
+# 	N = A.shape[1]
+# 	assert B.shape[1] == N
+
+# 	print(f"A.shape: {A.shape} B.shape: {B.shape}")
+
+# 	# calculate centroids
+# 	# A_centroid = np.reshape(1/N * (np.sum(A, axis=1)), (3,1))
+# 	# B_centroid = np.reshape(1/N * (np.sum(B, axis=1)), (3,1))
+
+# 	A_centroid = np.reshape(1/N * (np.sum(A, axis=0)), (1,3))
+# 	B_centroid = np.reshape(1/N * (np.sum(B, axis=0)), (1,3))
+
+
+# 	# calculate the vectors from centroids
+# 	A_prime = A - A_centroid
+# 	B_prime = B - B_centroid
+
+# 	print(f"A_prime and B_prime have been calculated!")
+
+# 	# rotation estimation
+# 	H = np.zeros([3, 3])
+# 	for i in range(N):
+# 		ai = A_prime[i, :]
+# 		bi = B_prime[i, :]
+# 		H = H + np.outer(ai, bi)
+# 	U, S, V_transpose = np.linalg.svd(H)
+# 	V = np.transpose(V_transpose)
+# 	U_transpose = np.transpose(U)
+# 	R = V @ np.diag([1, 1, np.linalg.det(V) * np.linalg.det(U_transpose)]) @ U_transpose
+
+# 	# translation estimation
+# 	t = B_centroid - R @ A_centroid
+
+# 	return R, t
 
 
 def get_pcl_aabb(pcd):
